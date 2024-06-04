85be34d3f3a3e7654de55a4768858891a2fedaec
Sooyoung <agriades23@gmail.com>
2024-06-04 13:53:44 +0900

    refactor: task 성공/실패 여부로 트러블슈팅 한꼬집 첨가 참고 자료: https://github.com/firebase/snippets-android/blob/b8f65e9150fe927a5f0473e15e16fa5803189b60/auth/app/src/main/java/com/google/firebase/quickstart/auth/kotlin/EmailPasswordActivity.kt#L28-L35

--- app/src/main/java/com/firebase/test/LogInActivity.kt
+++ app/src/main/java/com/firebase/test/LogInActivity.kt
@@ -3,7 +3,11 @@ package com.firebase.test
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.firebase.test.databinding.ActivityLogInBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
@@ -14,15 +18,17 @@ import com.google.firebase.ktx.Firebase
 
 class LogInActivity : Activity() {
     private lateinit var binding: ActivityLogInBinding
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLogInBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        auth = Firebase.auth
 
         binding.btnLogIn.setOnClickListener {
             val email = binding.etEmail.text.toString()
             val password = binding.etPassword.text.toString()
-            Firebase.auth.signInWithEmailAndPassword(email, password)
+            authLoginIfPossible(email, password) //firebase에 해당 이메일, 패스워드로 계정 생성
         }
 
         binding.btnSignUp.setOnClickListener {
@@ -31,5 +37,42 @@ class LogInActivity : Activity() {
         }
 
     }
+
+    private fun authLoginIfPossible(email: String, password: String){
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        baseContext,
+                        "Authentication failed.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                    updateUI(null)
+                }
+            }
+    }
+    override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            reload()
+        }
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+    }
+    private fun reload() {
+    }
+    companion object {
+        private const val TAG = "EmailPassword"
+    }
+
 
 }
--- app/src/main/java/com/firebase/test/SignUpActivity.kt
+++ app/src/main/java/com/firebase/test/SignUpActivity.kt
@@ -1,23 +1,29 @@
 package com.firebase.test
 
-import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.firebase.test.databinding.ActivitySignUpBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class SignUpActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignUpBinding
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        auth = Firebase.auth
 
         binding.btnSignUp.setOnClickListener {
             val email = binding.etEmail.text.toString()
             val password = binding.etPassword.text.toString()
-            Firebase.auth.createUserWithEmailAndPassword(email, password)
+            authSigninIfPossible(email, password) //firebase에서 해당 이메일, 비밀번호로 계정 생성 가능하면 생성
+
         }
 
         binding.btnBack.setOnClickListener {
@@ -25,4 +31,35 @@ class SignUpActivity : AppCompatActivity() {
         }
 
     }
+
+    private fun authSigninIfPossible(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        baseContext,
+                        "Authentication failed.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                    updateUI(null)
+                }
+            }
+    }
+    private fun updateUI(user: FirebaseUser?) {
+    }
+
+    private fun reload() {
+    }
+
+    companion object {
+        private const val TAG = "EmailPassword"
+    }
+
 }
