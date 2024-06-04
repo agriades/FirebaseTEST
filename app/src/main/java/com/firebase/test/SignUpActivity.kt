253eb2275280f7b7a5214b246a333cbba2341ab9
Sooyoung <agriades23@gmail.com>
2024-06-04 10:32:58 +0900

    draw: 회원가입 페이지 레이아웃 생성

---
+++ app/src/main/java/com/firebase/test/SignUpActivity.kt
@@ -0,0 +1,18 @@
+package com.firebase.test
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.firebase.test.databinding.ActivityLogInBinding
+import com.firebase.test.databinding.ActivitySignUpBinding
+
+class SignUpActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignUpBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
